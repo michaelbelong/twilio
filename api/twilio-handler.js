c1fde46409fa0400f051587af0fbfbf073c9aeff
michaelbelong <michaelbelong@gmail.com>
2023-08-18 14:30:35 -0700

    Second commit, now featuring a little ditty.

--- api/twilio-handler.js
+++ api/twilio-handler.js
@@ -2,7 +2,8 @@ module.exports = (req, res) => {
   res.setHeader('Content-Type', 'text/xml');
   res.status(200).send(`
 	<Response>
-		<Say voice="alice">Hello! This is your Twilio response from a Vercel serverless function.</Say>
+		<Say voice="alice">Hello! You fuckin' suck.</Say>
+		<Play>https://metanorm.com/MetanormFM-Context-Intro-v3.mp3</Play>
 	</Response>
   `);
 };
