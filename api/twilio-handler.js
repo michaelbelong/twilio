01b515abbbe0e57cbb49350b09750b6c774f1503
michaelbelong <michaelbelong@gmail.com>
2023-08-18 18:35:33 -0700

    Fifth commit. Okay.

--- api/twilio-handler.js
+++ api/twilio-handler.js
@@ -2,8 +2,8 @@ module.exports = (req, res) => {
   res.setHeader('Content-Type', 'text/xml');
   res.status(200).send(`
 	<Response>
-		<Say voice="alice">Hello! Welcome to the New Cope hotline! Please hold.</Say>
-		<Say voice="alice">We are currently experiencing higher than normal call volumes please stay on the line and we will answer your call in the order it arrived. Thank you.</Say>
+		<Say voice="Polly.Joanna">Hello! Welcome to the New Cope hotline!</Say>
+		<Say voice="Polly.Joanna">We are currently experiencing higher than normal call volumes please stay on the line and we will answer your call in the order it arrived. Thank you.</Say>
 		<Play>https://metanorm.com/OpusNo1.mp3</Play>
 	</Response>
   `);
